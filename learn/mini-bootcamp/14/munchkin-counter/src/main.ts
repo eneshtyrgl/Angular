80dbb56add07b87c33d45192295e0661b13e5d51
eneshtyrgl <eneshtyrgl@gmail.com>
2025-01-28 10:16:29 +0300

    add import for Angular animations in the main application bootstrap

--- learn/mini-bootcamp/14/munchkin-counter/src/main.ts
+++ learn/mini-bootcamp/14/munchkin-counter/src/main.ts
@@ -3,10 +3,12 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(HttpClientModule),
+    provideAnimations(),
     ...appConfig.providers,
   ],
 }).catch((err) => console.error(err));
